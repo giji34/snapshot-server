7677bdc90504d10699cb8c4095bddbc0581702b9
kbinani <kbinani.bt@gmail.com>
2020-03-21 22:48:37 +0900

    fix: getScheduler().runTaskTimer は deprecated なので代替の方法でタスクを起動する

--- tool/preparation/mod/src/main/java/com/github/giji34/worldgen/Main.java
+++ tool/preparation/mod/src/main/java/com/github/giji34/worldgen/Main.java
@@ -1,6 +1,9 @@
 package com.github.giji34.worldgen;
 
-import org.bukkit.*;
+import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
+import org.bukkit.GameMode;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -68,7 +71,6 @@ public class Main extends JavaPlugin implements Listener {
         int maxZ = Math.max(z0, z1);
         String version = args[4];
         final World world = player.getWorld();
-        Server server = getServer();
         GenerateTask task;
         try {
             task = new GenerateTask(world, minX, minZ, maxX, maxZ, version);
@@ -76,7 +78,7 @@ public class Main extends JavaPlugin implements Listener {
             player.sendMessage(ChatColor.RED + "タスクの起動エラー");
             return true;
         }
-        server.getScheduler().runTaskTimer(this, task, 10, 1);
+        task.runTaskTimer(this, 1, GenerateTask.kPeriod);
         this.task = task;
         player.sendMessage("タスクを起動しました");
         return true;
@@ -87,13 +89,15 @@ public class Main extends JavaPlugin implements Listener {
             player.sendMessage(ChatColor.RED + "実行中の generate コマンドはありません");
             return true;
         }
-        getServer().getScheduler().cancelTasks(this);
+        this.task.cancel();;
         this.task = null;
         player.sendMessage("タスクを終了させました");
         return true;
     }
 
     class GenerateTask extends BukkitRunnable {
+        static final long kPeriod = 20; // game ticks
+
         final int minX;
         final int minZ;
         final int maxX;
@@ -102,12 +106,17 @@ public class Main extends JavaPlugin implements Listener {
         boolean cancelSignaled;
         final String version;
         final int dimension;
+        final File indexDirectory;
 
         int x;
         int z;
         int numInspectedAfterLogged;
         int numGeneratedAfterLogged;
         long lastLoggedTimeMillis;
+
+        long start = -1;
+        long numInspectedTotal = 0;
+        long numGeneratedTotal = 0;
 
         GenerateTask(World world, int minX, int minZ, int maxX, int maxZ, String version) throws Exception {
             this.world = world;
@@ -135,6 +144,8 @@ public class Main extends JavaPlugin implements Listener {
                 default:
                     throw new Exception("");
             }
+            File jar = getFile();
+            this.indexDirectory = new File(new File(new File(new File(jar.getParent(), "giji34"), "wildblocks"), version), Integer.toString(dimension));
         }
 
         @Override
@@ -142,16 +153,17 @@ public class Main extends JavaPlugin implements Listener {
             if (x > maxX || this.cancelSignaled) {
                 return;
             }
-            long start = System.currentTimeMillis();
-            long msPerTick = 1000 / 20;
-            long maxElapsedMS = 5 * msPerTick;
+            final long start = System.currentTimeMillis();
+            if (this.start < 0) {
+              this.start = start;
+            }
+            final long msPerTick = 1000 / 20;
+            final long maxElapsedMS = kPeriod * msPerTick;
             int count = 0;
-            File jar = getFile();
-            File directory = new File(new File(new File(new File(jar.getParent(), "giji34"), "wildblocks"), version), Integer.toString(dimension));
             int generated = 0;
             while (!cancelSignaled) {
                 String name = "c." + x + "." + z + ".idx";
-                File idxFile = new File(directory, name);
+                File idxFile = new File(indexDirectory, name);
                 if (!idxFile.exists()) {
                     Chunk chunk = world.getChunkAt(x, z);
                     chunk.load(true);
@@ -165,7 +177,7 @@ public class Main extends JavaPlugin implements Listener {
                     z = minZ;
                     x += 1;
                     if (x > maxX) {
-                        System.out.println("finished");
+                        getLogger().info("finished");
                         break;
                     }
                 }
@@ -175,20 +187,33 @@ public class Main extends JavaPlugin implements Listener {
             }
             this.numInspectedAfterLogged += count;
             this.numGeneratedAfterLogged += generated;
+            this.numInspectedTotal += count;
+            this.numGeneratedTotal += generated;
             long end = System.currentTimeMillis();
             if (end - this.lastLoggedTimeMillis > 10 * 1000) {
-              System.out.println("[" + x + ", " + z + "] " + this.numInspectedAfterLogged + " chunks inspected, " + this.numGeneratedAfterLogged + " chunks generated");
-              this.numInspectedAfterLogged = 0;
-              this.numGeneratedAfterLogged = 0;
-              this.lastLoggedTimeMillis = end;
+              this.logStatistics();
             }
+        }
+
+        private void logStatistics() {
+          final long now = System.currentTimeMillis();
+          final double sec = (now - this.start) / 1000.0;
+          final double progress = this.numInspectedTotal * 100.0 / (double)((this.maxX - this.minX + 1) * (this.maxZ - this.minZ + 1));
+          getLogger().info("[" + x + ", " + z + "] "
+            + "(" + progress + "%) "
+            + this.numInspectedTotal + " inspected (+" + this.numInspectedAfterLogged + ", " + (this.numInspectedTotal / sec) + " [chunk/s]), "
+            + this.numGeneratedTotal + " generated (+" + this.numGeneratedAfterLogged + ", " + (this.numGeneratedTotal / sec) + " [chunk/s])");
+          this.numInspectedAfterLogged = 0;
+          this.numGeneratedAfterLogged = 0;
+          this.lastLoggedTimeMillis = now;
         }
 
         @Override
         public void cancel() {
-            System.out.println("cancel called");
+            getLogger().info("cancel");
             super.cancel();
             this.cancelSignaled = true;
+            this.logStatistics();
         }
     }
 
