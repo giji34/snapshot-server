0609464164a20edb14f67a9d8fd7449bc64119e6
kbinani <kbinani.bt@gmail.com>
2020-03-22 16:11:18 +0900

    [preparation/mod] cleanup: GenerateTask を別ファイルで定義

--- tool/preparation/mod/src/main/java/com/github/giji34/worldgen/Main.java
+++ tool/preparation/mod/src/main/java/com/github/giji34/worldgen/Main.java
@@ -1,7 +1,6 @@
 package com.github.giji34.worldgen;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Chunk;
 import org.bukkit.GameMode;
 import org.bukkit.World;
 import org.bukkit.command.Command;
@@ -9,13 +8,12 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 
 public class Main extends JavaPlugin implements Listener {
-    private GenerateTask task;
+    private WorldGenTask task;
 
     @Override
     public void onLoad() {
@@ -71,14 +69,16 @@ public class Main extends JavaPlugin implements Listener {
         int maxZ = Math.max(z0, z1);
         String version = args[4];
         final World world = player.getWorld();
-        GenerateTask task;
+        File jar = getFile();
+        File dbDirectory = new File(new File(jar.getParent(), "giji34"), "wildblocks");
+        WorldGenTask task;
         try {
-            task = new GenerateTask(world, minX, minZ, maxX, maxZ, version);
+            task = new WorldGenTask(getLogger(), dbDirectory, world, minX, minZ, maxX, maxZ, version);
         } catch (Exception e) {
             player.sendMessage(ChatColor.RED + "タスクの起動エラー");
             return true;
         }
-        task.runTaskTimer(this, 1, GenerateTask.kPeriod);
+        task.runTaskTimer(this, 1, WorldGenTask.kPeriodTicks);
         this.task = task;
         player.sendMessage("タスクを起動しました");
         return true;
@@ -93,128 +93,6 @@ public class Main extends JavaPlugin implements Listener {
         this.task = null;
         player.sendMessage("タスクを終了させました");
         return true;
-    }
-
-    class GenerateTask extends BukkitRunnable {
-        static final long kPeriod = 20; // game ticks
-
-        final int minX;
-        final int minZ;
-        final int maxX;
-        final int maxZ;
-        final World world;
-        boolean cancelSignaled;
-        final String version;
-        final int dimension;
-        final File indexDirectory;
-
-        int x;
-        int z;
-        int numInspectedAfterLogged;
-        int numGeneratedAfterLogged;
-        long lastLoggedTimeMillis;
-
-        long start = -1;
-        long numInspectedTotal = 0;
-        long numGeneratedTotal = 0;
-
-        GenerateTask(World world, int minX, int minZ, int maxX, int maxZ, String version) throws Exception {
-            this.world = world;
-            this.minX = minX;
-            this.minZ = minZ;
-            this.maxX = maxX;
-            this.maxZ = maxZ;
-            this.x = minX;
-            this.z = minZ;
-            this.cancelSignaled = false;
-            this.version = version;
-            this.numInspectedAfterLogged = 0;
-            this.numGeneratedAfterLogged = 0;
-            this.lastLoggedTimeMillis = 0;
-            switch (world.getEnvironment()) {
-                case NETHER:
-                    this.dimension = -1;
-                    break;
-                case THE_END:
-                    this.dimension = 1;
-                    break;
-                case NORMAL:
-                    this.dimension = 0;
-                    break;
-                default:
-                    throw new Exception("");
-            }
-            File jar = getFile();
-            this.indexDirectory = new File(new File(new File(new File(jar.getParent(), "giji34"), "wildblocks"), version), Integer.toString(dimension));
-        }
-
-        @Override
-        public void run() {
-            if (x > maxX || this.cancelSignaled) {
-                return;
-            }
-            final long start = System.currentTimeMillis();
-            if (this.start < 0) {
-              this.start = start;
-            }
-            final long msPerTick = 1000 / 20;
-            final long maxElapsedMS = kPeriod * msPerTick;
-            int count = 0;
-            int generated = 0;
-            while (!cancelSignaled) {
-                String name = "c." + x + "." + z + ".idx";
-                File idxFile = new File(indexDirectory, name);
-                if (!idxFile.exists()) {
-                    Chunk chunk = world.getChunkAt(x, z);
-                    chunk.load(true);
-                    chunk.unload(true);
-                    generated++;
-                }
-                count++;
-                long elapsed = System.currentTimeMillis() - start;
-                z += 1;
-                if (z > maxZ) {
-                    z = minZ;
-                    x += 1;
-                    if (x > maxX) {
-                        getLogger().info("finished");
-                        break;
-                    }
-                }
-                if (elapsed > maxElapsedMS) {
-                    break;
-                }
-            }
-            this.numInspectedAfterLogged += count;
-            this.numGeneratedAfterLogged += generated;
-            this.numInspectedTotal += count;
-            this.numGeneratedTotal += generated;
-            long end = System.currentTimeMillis();
-            if (end - this.lastLoggedTimeMillis > 10 * 1000) {
-              this.logStatistics();
-            }
-        }
-
-        private void logStatistics() {
-          final long now = System.currentTimeMillis();
-          final double sec = (now - this.start) / 1000.0;
-          final double progress = this.numInspectedTotal * 100.0 / (double)((this.maxX - this.minX + 1) * (this.maxZ - this.minZ + 1));
-          getLogger().info("[" + x + ", " + z + "] "
-            + "(" + progress + "%) "
-            + this.numInspectedTotal + " inspected (+" + this.numInspectedAfterLogged + ", " + (this.numInspectedTotal / sec) + " [chunk/s]), "
-            + this.numGeneratedTotal + " generated (+" + this.numGeneratedAfterLogged + ", " + (this.numGeneratedTotal / sec) + " [chunk/s])");
-          this.numInspectedAfterLogged = 0;
-          this.numGeneratedAfterLogged = 0;
-          this.lastLoggedTimeMillis = now;
-        }
-
-        @Override
-        public void cancel() {
-            getLogger().info("cancel");
-            super.cancel();
-            this.cancelSignaled = true;
-            this.logStatistics();
-        }
     }
 
     private boolean invalidGameMode(Player player) {
